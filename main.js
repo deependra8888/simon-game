27c461bb0c477df41cc2d0d324add4852238ebd0
Deependra Rathore <deependre****gmail.com>
2023-05-28 11:53:14 +0530

    simon game

---
+++ main.js
@@ -0,0 +1,42 @@
+
+
+// const readlineSync = require("readline-sync");
+
+let colors = ["green", "red", "yellow", "blue"];
+
+const randomColor = () => {
+  let randomIndex = Math.random() * colors.length;
+  let newColor = colors[Math.floor(randomIndex)];
+  return newColor;
+};
+
+function play(simon, player) {
+    console.log('simon : '  + simon);
+    console.log('player : ' + player)
+  if (simon.length === player.length) {
+    for (let color in simon) {
+      if (simon[color] !== player[color]) {
+        console.log('gameOver')
+        return ;
+      }
+    }
+    let newColor = randomColor();
+    simon.push(newColor);
+    play(simon, player);
+  } else {
+    let newPlayer = [];
+    for (let color in simon) {
+    //   let playerColor = readlineSync.question(`\n enter color ${+color + 1} = `);
+    
+      newPlayer.push(playerColor)
+    }
+    play(simon, newPlayer);
+  }
+
+}
+
+play([], []);
+
+let 
+
+
