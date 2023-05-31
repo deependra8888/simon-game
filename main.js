83382443e06cb4b5ee089ee4212458a7f241f389
Archis <92965519+archisvaze@users.noreply.github.com>
2023-05-31 16:55:11 +0530

    game done!!!

--- main.js
+++ main.js
@@ -1,118 +1,92 @@
-let green = document.querySelector(".green");
-let red = document.querySelector(".red");
-let blue = document.querySelector(".blue");
-let yellow = document.querySelector(".yellow");
-green.addEventListener("click", pushColorToPlayer);
-blue.addEventListener("click", pushColorToPlayer);
-red.addEventListener("click", pushColorToPlayer);
-yellow.addEventListener("click", pushColorToPlayer);
-let startBtn = document.querySelector(".start-button");
+let green = document.querySelector('.green');
+let red = document.querySelector('.red');
+let blue = document.querySelector('.blue');
+let yellow = document.querySelector('.yellow');
+green.addEventListener('click', pushColorToPlayer);
+blue.addEventListener('click', pushColorToPlayer);
+red.addEventListener('click', pushColorToPlayer);
+yellow.addEventListener('click', pushColorToPlayer);
 
-let colors = ["green", "red", "blue", "yellow"];
+let startBtn = document.querySelector('.start-button');
+startBtn.addEventListener('click', startGame);
+
+let colors = ['green', 'red', 'blue', 'yellow'];
 
 let player = [];
 let simon = [];
-let turn = "simon";
+let turn = '';
 
 const randomColor = () => {
-  let randomIndex = Math.random() * colors.length;
-  let newColor = colors[Math.floor(randomIndex)];
-  return newColor;
+    let randomIndex = Math.random() * colors.length;
+    let newColor = colors[Math.floor(randomIndex)];
+    return newColor;
 };
 
-
-
 function glow(colorElm, indx) {
-  colorElm.classList.add("glow");
-  turn = "simon";
-  setTimeout(() => {
-    colorElm.classList.remove("glow");
-    if (simon.length === indx + 1) {
-      turn = "player";
-    }
-
-    console.log(turn);
-  }, 500);
-  console.log(turn);
+    setTimeout(() => {
+        colorElm.classList.add('glow');
+        turn = 'simon';
+        setTimeout(() => {
+            colorElm.classList.remove('glow');
+            if (indx === simon.length - 1) {
+                turn = 'player';
+                //reset player
+                player = [];
+                console.log(`It is ${turn}'s turn now!`);
+            }
+        }, 500);
+    }, 1000);
 }
 
-function play(simon, player) {
-//   removeAllEventListners();
-  console.log("simon : " + simon);
-  console.log("player : " + player);
-  if (turn === "simon") {
-    simon.push(randomColor());
-  }
-  simon.forEach((color, index) => {
-    setTimeout(() => {
-      console.log();
-      let elm = document.querySelector(`.${color}`);
-      glow(elm, index);
-    }, (index + 1) * 200);
-  });
-
-  setTimeout(() => {
-    if (turn === "player") {
-     
+function play() {
+    if (turn === 'simon' || turn === '') {
+        simon.push(randomColor());
+        console.log('SIMONS  = ', simon);
     }
-  }, (simon.length + 1) + 1000);
+    simon.forEach((color, index) => {
+        setTimeout(() => {
+            console.log();
+            let elm = document.querySelector(`.${color}`);
+            glow(elm, index);
+        }, (index + 1) * 1000);
+    });
 }
 
 function pushColorToPlayer(event) {
-  if(turn === 'player'){
-    player.push(event.target.classList[0]);
-    console.log(player);
-    check(simon, player);
-  }
+    console.log('CLICK!');
+    if (turn === 'player') {
+        player.push(event.target.classList[0]);
+        check();
+    }
 }
 
-function check(simon, player) {
-  console.log("checking...");
-  if (simon.length !== player.length) {
-    console.log("waiting...");
-    console.log("I have clicked");
-    // return false;
-  }
-  for (let color in simon) {
-    if (simon[color] !== player[color]) {
-      console.log("gameover");
-      return "gameOver";
+function check() {
+    console.log('checking player...');
+    console.log('PLAYERS  = ', player);
+    if (simon.length !== player.length) {
+        console.log('Lengths dont match, waiting...');
     }
-  }
-
-  console.log("next level....");
-  turn = "simon";
-  console.log(simon.length);
-//   removeAllEventListners();
-  play(simon, player);
+    if (simon.length === player.length) {
+        for (let color in simon) {
+            if (simon[color] !== player[color]) {
+                console.log('NOT MATCHED!!!  :(');
+                console.log('Game Over!');
+                turn = '';
+                return;
+            }
+        }
+        console.log('MATCHED!!! :D');
+        console.log(`LEVEL:  ${simon.length + 1}`);
+        turn = 'simon';
+        play();
+    }
 }
 
 function startGame() {
-//   removeAllEventListners();
-  console.log("starting game....");
-  player = [];
-  simon = [];
-  play(simon, player);
+    if (!turn) {
+        console.log('Starting Game....');
+        player = [];
+        simon = [];
+        play();
+    }
 }
-
-function removeAllEventListners() {
-  green.removeEventListener("click", pushColorToPlayer);
-  blue.removeEventListener("click", pushColorToPlayer);
-  red.removeEventListener("click", pushColorToPlayer);
-  yellow.removeEventListener("click", pushColorToPlayer);
-}
-
-console.log(startBtn);
-
-startBtn.addEventListener("click", startGame);
-
-
-// step 1 => startGame simon = [] and player = [] and call play
-// step 2 => push a random color to simon and glow the color 
-
-// step 3 =>  once glow is over add eventListners 
-// step 4 => wait for the user input and as soon as user clicks remove eventListners
-
-
-
-
